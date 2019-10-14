35dfa86510b477ef8807ab15d4f44b4adcadefd7
Angus McLeod <angus@mcleod.org.au>
2019-10-14 17:30:43 +1100

    Only create serialized user if there are additional attributes

--- app/serializers/directory_item_user_serializer.rb
+++ app/serializers/directory_item_user_serializer.rb
@@ -7,11 +7,14 @@ class DirectoryItemUserSerializer < UserNameSerializer
     attrs = super
     
     additional_attrs = SiteSetting.user_directory_additional_attributes.split("|")
-    serialized_user = UserSerializer.new(object, scope: scope)
+    
+    if additional_attrs.any?
+      serialized_user = UserSerializer.new(object, scope: scope)
 
-    additional_attrs.each do |attr|
-      if serialized_user.respond_to?("include_#{attr}?") && serialized_user.send("include_#{attr}?")
-        attrs[attr.to_sym] = serialized_user.send(attr)
+      additional_attrs.each do |attr|
+        if serialized_user.respond_to?("include_#{attr}?") && serialized_user.send("include_#{attr}?")
+          attrs[attr.to_sym] = serialized_user.send(attr)
+        end
       end
     end
     
